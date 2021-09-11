8367cd003be61e650356bdf828fc3c3ab134b165
supratim dey <supratimdey@outlook.com>
2021-09-12 00:38:04 +0530

    done incroporating consumer of provider

--- flutter_exp/lib/widgets/transaction_list.dart
+++ flutter_exp/lib/widgets/transaction_list.dart
@@ -6,82 +6,86 @@ import '../model/transaction.dart';
 import '../transaction_data.dart';
 
 class TransactionList extends StatelessWidget {
-  // const TransactionList({
-  //   Key? key,
-  //   required this.transactions,
-  // }) : super(key: key);
+  const TransactionList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final List<Transaction> transactions =
-        Provider.of<TransactionData>(context).transactions;
-    return Column(
-      children: transactions.map(
-        (tx) {
-          return Card(
-            color: Colors.white,
-            child: Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      //color: Colors.b,
-                      width: 2,
+    return Consumer<TransactionData>(builder: (context, txData, child) {
+      return Column(
+        children:
+            txData.transactions.map((tx) => TransactionCard(tx: tx)).toList(),
+      );
+    });
+  }
+}
+
+class TransactionCard extends StatelessWidget {
+  final Transaction tx;
+  const TransactionCard({Key? key, required this.tx}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                //color: Colors.b,
+                width: 2,
+              ),
+            ),
+            child: Center(
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.price_change_outlined,
+                    color: Colors.green,
+                    size: 24,
+                  ),
+                  Text(
+                    "\$ ${tx.amount.toString()}",
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple,
                     ),
                   ),
-                  // width: 80,
-                  // height: 60,
-                  child: Center(
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.price_change_outlined,
-                          color: Colors.green,
-                          size: 24,
-                        ),
-                        Text(
-                          "\$ ${tx.amount.toString()}",
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ],
-                    ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  tx.title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black87,
-                        ),
-                      ),
-                      Text(DateFormat.yMMMMd().format(tx.date),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey,
-                          )),
-                    ],
+                Text(
+                  DateFormat.yMMMMd().format(tx.date),
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
                   ),
                 ),
               ],
             ),
-            elevation: 10,
-          );
-        },
-      ).toList(),
+          ),
+        ],
+      ),
+      elevation: 10,
     );
   }
 }
